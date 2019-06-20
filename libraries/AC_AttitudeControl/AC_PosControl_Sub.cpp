4f21ec5ec49f63af726aa39a102fab0c9da2f762
Nils Dunkelberg <nils.dunkelberg2@gmail.com>
2019-06-20 14:54:38 +0200

    [ArduSub] allow manual maneouvres during nettracking

--- libraries/AC_AttitudeControl/AC_PosControl_Sub.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl_Sub.cpp
@@ -108,15 +108,17 @@ void AC_PosControl_Sub::relax_alt_hold_controllers()
     _pid_accel_z.reset_filter();
 }
 
-void AC_PosControl_Sub::update_dist_controller(float& target_forward, float distance_error, float dt)
+void AC_PosControl_Sub::update_dist_controller(float& target_forward, float distance_error, float dt, bool update)
 {
     // simple pid controller for distance control
     // todo: check if useful to use implemented xy-position controller
     float p, i, d;
 
-    _pid_dist.set_dt(dt);
-
-    _pid_dist.set_input_filter_all(distance_error);
+    if (update)
+    {
+        _pid_dist.set_dt(dt);
+        _pid_dist.set_input_filter_all(distance_error);
+    }
 
     // separately calculate p, i, d values for logging
     // probably better to use square root constrain
